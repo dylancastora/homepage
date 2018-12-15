70e2b0e4ac86ab609e9f3c0e3f9f5339869aabbf
Dylan Castora <dcastora@Dylans-MBP.hsd1.ma.comcast.net>
2018-12-14 20:35:13 -0500

    Connect api and frontend

---
+++ vue/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class='hello'>
+    <div class='container-fluid'>
+      <div 
+      v-for='project in projects'
+      :key='project.nid[0].value'>
+        <h2>{{project.title[0].value}}</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+const apiURL = 'http://drupal.docker.localhost:8000/api/projects?_format=json'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      projects: []
+    }
+  },
+  mounted () {
+    axios
+      .get(apiURL)
+      .then(response => (this.projects = response.data))
+  }
+}
+</script>
